9e2d2ff366956d89ab75236b6ee633d283112ed4
Abhyan Morkal <61055763+abhyanmorkal@users.noreply.github.com>
2024-07-05 22:27:03 +0530

    file importent problem solve

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,7 +1,11 @@
 import { View, Text } from "react-native";
 import React from "react";
+import data from "~/assets/data/post.json";
 
+const post1 = data[0];
 export default function TabsIndex() {
+  console.log(post1);
+
   return (
     <View className=" flex-1 bg-purple-400">
       <Text className=" font-bold text-xl text-blue-500">TabsIndex</Text>
